4a0a1aa8780b6c26eb17afbdec2cf1ead8d200db
Gerald Orban <gerald@blackbirdinteractive.com>
2013-04-01 14:04:16 -0700

    Error tolerant JSON parsing for motor controller telemetry.
    On telemetry update, the motor controller will post events to listeners.

--- ProsthesisOS/Arduino Communications Library/MotorControllerArduino.cs
+++ ProsthesisOS/Arduino Communications Library/MotorControllerArduino.cs
@@ -9,12 +9,28 @@ namespace ArduinoCommunicationsLibrary
 {
     public sealed class MotorControllerArduino : ArduinoCommsBase
     {
+        public event Action<ProsthesisTelemetry.ProthesisMotorTelemetry> TelemetryUpdate = null;
         public const string kArduinoID = "mcon";
         public MotorControllerArduino(ProsthesisCore.Utility.Logger logger) : base(kArduinoID, logger) { }
 
+        private ProsthesisTelemetry.ProthesisMotorTelemetry mCurrentState = null;
+        public ProsthesisTelemetry.ProthesisMotorTelemetry DeviceState { get { return mCurrentState; } }
+
         protected override void OnTelemetryReceive(string telemetryData)
         {
-            ProsthesisTelemetry.ProthesisMotorTelemetry motorTelem = Newtonsoft.Json.JsonConvert.DeserializeObject<ProsthesisTelemetry.ProthesisMotorTelemetry>(telemetryData);
+            try
+            {
+                ProsthesisTelemetry.ProthesisMotorTelemetry motorTelem = Newtonsoft.Json.JsonConvert.DeserializeObject<ProsthesisTelemetry.ProthesisMotorTelemetry>(telemetryData);
+                mCurrentState = motorTelem;
+                if (TelemetryUpdate != null)
+                {
+                    TelemetryUpdate(mCurrentState);
+                }
+            }
+            catch (Exception e)
+            {
+                mLogger.LogMessage(ProsthesisCore.Utility.Logger.LoggerChannels.Arduino, string.Format("AID:{0} failed to parse JSON \"{1}\" because of {2}", mArduinoID, telemetryData, e));
+            }
         }
     }
 }
